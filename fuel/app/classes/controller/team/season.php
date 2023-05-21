c50b0c54c0ef5c72ac23e6d40b81895b40bc2561
Patrick Foltz <historian@racerhistory.com>
2023-05-21 16:54:50 -0500

    common display functions split out to single class

--- fuel/app/classes/controller/team/season.php
+++ fuel/app/classes/controller/team/season.php
@@ -30,41 +30,10 @@ class Controller_Team_Season extends Controller_Template {
         
         $this->template->set_global('team_season', $data, false);
         $this->template->set_global('record', Model_Game::getRecords('team', 'season', $id));
-        $this->template->set_global('stats', $this->getStats($id), false);
-        
-        $this->template->title = ($data->semester == 1 ? $data->seasons->start : ($data->semester == 2 ? $data->seasons->start.'-'.$data->seasons->end : $data->seasons->end).' - '.$data->teams->team_name);
+        $this->template->set_global('stats', Data_Seasonview::getStats($id), false);        
+        $this->template->set_global('title', $data->semester == 1 ? $data->seasons->start : ($data->semester == 2 ? $data->seasons->start.'-'.$data->seasons->end : $data->seasons->end).' - '.$data->teams->team_name);
         
         $this->template->content = View::forge('team/season/view');
     }
     
-    private static function getStats($season) {
-        $stats = [];
-        $query_team = Model_Stat_Basket_Season_Team::query()->where('team_season_id', $season);
-        $query_person = Model_Team_Season_Roster::query()->related('stat_basket_season_person')->where('team_season_id', $season)->order_by('stat_basket_season_person.PTS', 'DESC');
-//      add season opponent stats
-        if ($query_person->count() > 0) {
-            foreach ($query_person->get() as $result) {
-                if ($result->stat_basket_season_person) {
-                    foreach ($result->stat_basket_season_person as $key => $value) {
-                        if ($value >= 0) {
-                            $stats[$key] = true;
-                        }
-                    }
-                }    
-            }
-        }
-        $stats['person'] = $query_person->get();
-        if ($query_team->count() > 0) {
-            foreach ($query_team->get() as $result) {
-                foreach ($result as $key => $value) {
-                    if ($value >= 0) {
-                        $stats[$key] = true;
-                    }
-                }
-            }
-        }
-
-        return $stats;
-    }
-
 }
---
+++ fuel/app/classes/data/seasonview.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Data_Seasonview
+ *
+ * @author Patrick Foltz
+ */
+class Data_Seasonview {
+    
+    public static function getStats($season) {
+        $stats = [];
+        $query_team = Model_Stat_Basket_Season_Team::query()->where('team_season_id', $season);
+        $query_person = Model_Team_Season_Roster::query()->related('stat_basket_season_person')->where('team_season_id', $season)->order_by('stat_basket_season_person.PTS', 'DESC');
+        if ($query_person->count() > 0) {
+            foreach ($query_person->get() as $result) {
+                if ($result->stat_basket_season_person) {
+                    foreach ($result->stat_basket_season_person as $key => $value) {
+                        if ($value >= 0) {
+                            $stats[$key] = true;
+                        }
+                    }
+                }    
+            }
+        }
+        $stats['person'] = $query_person->get();
+        if ($query_team->count() > 0) {
+            foreach ($query_team->get() as $result) {
+                foreach ($result as $key => $value) {
+                    if ($value >= 0) {
+                        $stats[$key] = true;
+                    }
+                }
+            }
+        }
+
+        return $stats;
+    }
+}
