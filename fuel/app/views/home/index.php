d9f3a2d05e86a0c46ffc08ba3e5c5f07aefc665a
Patrick Foltz <historian@racerhistory.com>
2023-07-23 10:19:52 -0500

    Landing page

--- fuel/app/views/home/index.php
+++ fuel/app/views/home/index.php
@@ -1,5 +1,31 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "index" ); ?>'><?php echo Html::anchor('home/index','Index');?></li>
-
-</ul>
-<p>Index</p>
+<h1>Welcome to Racerhistory.com!</h1>
+<p>There has been some major changes around here, a complete overhaul as a matter of fact. 
+You can still find all of the player stats and season history you could before, but in (what I hope) is a more pleasant way of interacting with it and with greater detail than ever before!&nbsp;</p>
+<h2>What's New?</h2>
+<h4><span style="text-decoration: underline;">More stats! </span></h4>
+<p>I've added the ability to provide complete game box scores (as they are entered)</p>
+<p><img class="" width="711" height="456" title="I'm so happy with the way this turned out, you have no idea" alt="Game View Iamge" src="/assets/img/Racerhistorygame.png"></img>
+</p>
+<p>I'll be adding more games in, but there is a LOT and it's going to take some time. I've got newspaper box scores going back all the way to 1926 that I'll be adding.</p>
+<h4><span style="text-decoration: underline;">Administrative Backend</span></h4>
+<p>What else is new is something that you won't see. With the overhaul came the creation of an administrative backend that will make it much easier to keep things up to date, and add additional content not previously available.</p>
+<h2>What's Missing?</h2>
+<h4><span style="text-decoration: underline;">Images, stories, and biographies. </span></h4>
+<p>They aren't gone forever, I just haven't finished that part of the site. I've no timetable for their return.</p>
+<h2>What's Next?</h2>
+<h4><span style="text-decoration: underline;">More Stats</span></h4>
+<p>As mentioned above, and once I start getting the game box scores in I'll be adding in single game stats to the stat search pages.&nbsp;</p>
+<h4><span style="text-decoration: underline;">Game Search</span></h4>
+<p>A game search feature similar to the stat search where you'll be able to quickly find the largest loss on the road to a conference opponent on a Tuesday.</p>
+<h4><span style="text-decoration: underline;">Views by Opponent, Place, or Person</span></h4>
+<p>All games, records, and stats Vs an opponent, in an arena, city, state, or involving a person with a click.</p>
+<h4><span style="text-decoration: underline;">Women's Stats</span></h4>
+<p>Women's basketball belongs here too and I plan on bringing them here. With that will be the ability to include them in all of the above searches and views.</p>
+<h2>The Far Future</h2>
+<h4><span style="text-decoration: underline;">Additional Sports</span></h4>
+<p>I mean, why stop at basketball??</p>
+<p>When I set out to build the new Racerhistory.com I did it with three goals in mind:</p>
+<p style="padding-left: 40px;">Easier to Update</p>
+<p style="padding-left: 40px;">Better Search Capabilities</p>
+<p style="padding-left: 40px;">Include More Sports</p>
+<p>The foundation is built, but getting Basketball ironed out will go a long way to making this future goal a reality. I think I'll go with Baseball &amp; Softball next as they share stats so the views and stats will use the same code, then Football, Soccer, and Volleyball (can you dig it?)</p>
